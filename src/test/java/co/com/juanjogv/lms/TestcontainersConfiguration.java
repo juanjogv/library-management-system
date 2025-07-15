8e89df6fb122247cfb103a3f02d493617b175ae1
Juan Jose Garcia <juangarciavxx@gmail.com>
2025-07-15 12:07:13 -0500

    Adds initial project structure

---
+++ src/test/java/co/com/juanjogv/lms/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package co.com.juanjogv.lms;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
